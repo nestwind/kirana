28abdda53bc2c4d1413d8ff65a778c832c901727
Bitangsha Ray <bitangsha2014@gmail.com>
2021-02-08 13:54:36 +0530

    First Commit
    Uploading all files

---
+++ kirana-store-ws/src/main/java/com/kirana/KiranaStoreWsApplication.java
@@ -0,0 +1,13 @@
+package com.kirana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaStoreWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaStoreWsApplication.class, args);
+	}
+
+}
