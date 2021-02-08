7e05e1e3cfd4a13e73e1305f3abfc29c3310221f
Bitangsha Ray <bitangsha2014@gmail.com>
2021-02-08 14:00:35 +0530

    First Commit

---
+++ kirana-store/src/main/java/com/kirana/KiranaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kirana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaStoreApplication.class, args);
+	}
+
+}
---
+++ kirana-store/src/test/java/com/kirana/KiranaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
