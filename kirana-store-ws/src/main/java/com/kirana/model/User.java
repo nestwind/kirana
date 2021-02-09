5b21e45b340d5b90689363c0bbe48c37116d7d9a
Bitangsha Ray <bitangsha2014@gmail.com>
2021-02-09 11:06:36 +0530

    1. Added web jars of bootstrap and jquery 2. Testing thymeleaf 3. Created index controller

--- kirana-store-ws/src/main/java/com/kirana/model/User.java
+++ kirana-store-ws/src/main/java/com/kirana/model/User.java
@@ -1,10 +1,8 @@
 package com.kirana.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
